cc14e811eb766867a41e18479c472ec25bce2514
AryaWeronika <74681098+AryaWeronika@users.noreply.github.com>
2022-04-12 14:57:28 +0200

    Add files via upload

---
+++ Frontend-4/main.js
@@ -0,0 +1,69 @@
+const steps = Array.from(document.querySelectorAll("form .step"));
+const nextBtn = document.querySelectorAll("form .next-btn");
+const prevBtn = document.querySelectorAll("form .previous-btn");
+const form = document.querySelector("form");
+
+nextBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    if(true){ // check condition if something was filled
+      changeStep("next");
+    } else {
+      window.alert("sometext");
+    }
+  });
+});
+prevBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    changeStep("prev");
+  });
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputs = []; // results initialized to null
+  form.querySelectorAll("input").forEach((input) => {
+    // if input value is selected
+    // set equal to var input
+    // push to list
+    if(input.checked){
+      const { name, id, value } = input;
+      inputs.push({ name, id, value });
+    }
+    if(input.type != "radio" && input.type != "checkbox"){
+      const { name, id, value } = input;
+      inputs.push({ name, id, value });
+    }
+  });
+  console.log(inputs);
+  form.reset();
+});
+
+
+
+
+function changeStep(btn) {
+  let index = 0;
+  const active = document.querySelector(".active");
+  index = steps.indexOf(active);
+  steps[index].classList.remove("active");
+  if (btn === "next") {
+    index++;
+  }
+  else if (btn === "prev") {
+    index--;
+  }
+
+  steps[index].classList.add("active");
+}
+
+const imieField = document.querySelector("input");
+
+nameField.addEventListener("input", () => {
+  nameField.setCustomValidity("");
+  nameField.checkValidity();
+  console.log(nameField.checkValidity());
+});
+
+nameField.addEventListener("invalid", () => {
+  nameField.setCustomValidity("Please fill in your First Name.");
+});
