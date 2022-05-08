846d2589762b8125286a36dfca08bcd6eabcf87d
Redziok <59537524+Redziok@users.noreply.github.com>
2022-05-08 22:19:54 +0200

    db connected

--- backend/Pages/ValuesController.cs
+++ backend/Pages/ValuesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using Npgsql;
 using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -28,13 +29,37 @@ namespace Strona_internetowa.Pages
         [HttpPost]
         public void Post([FromBody] Ankieta value)
         {
-            if (value == null) { }
-            /*using (var context = new BloggingContext())
-            {
-                var blog = new Blog { Url = "http://example.com/" };
-                context.Blogs.Add(blog);
-                context.SaveChanges();
-            }*/
+            var cs = "Host=localhost:5432;Username=postgres;Password=Redziok;Database=postgres";
+
+            using var con = new NpgsqlConnection(cs);
+            con.Open();
+
+            var sql = "INSERT INTO public.ankieta(firstname, age, height, weight, work, hoursworked, hoursrelaxed, activityamount, favouriteactivity, alcoholusage, cigaretteusage, coffeeusage, sleepquality, hoursslept, energyquality, sickness, allergy, healthproblems, medicationintake, foodavoided) VALUES (@firstname, @age, @height, @weight, @work, @hoursworked, @hoursrelaxed, @activityamount, @favouriteactivity, @alcoholusage, @cigaretteusage, @coffeeusage, @sleepquality, @hoursslept, @energyquality, @sickness, @allergy, @healthproblems, @medicationintake, @foodavoided)";
+            using var cmd = new NpgsqlCommand(sql, con);
+
+            cmd.Parameters.AddWithValue("firstname", value.FirstName);
+            cmd.Parameters.AddWithValue("age", value.Age);
+            cmd.Parameters.AddWithValue("height", value.Height);
+            cmd.Parameters.AddWithValue("weight", value.Weight);
+            cmd.Parameters.AddWithValue("work",value.Work);
+            cmd.Parameters.AddWithValue("hoursworked", value.HoursWorked);
+            cmd.Parameters.AddWithValue("hoursrelaxed", value.HoursRelaxed);
+            cmd.Parameters.AddWithValue("activityamount", value.ActivityAmount);
+            cmd.Parameters.AddWithValue("favouriteactivity", value.FavouriteActivity);
+            cmd.Parameters.AddWithValue("alcoholusage", value.AlcoholUsage);
+            cmd.Parameters.AddWithValue("cigaretteusage", value.CigaretteUsage);
+            cmd.Parameters.AddWithValue("coffeeusage", value.CoffeeUsage);
+            cmd.Parameters.AddWithValue("sleepquality", value.SleepQuality);
+            cmd.Parameters.AddWithValue("hoursslept", value.HoursSlept);
+            cmd.Parameters.AddWithValue("energyquality", value.EnergyQuality);
+            cmd.Parameters.AddWithValue("sickness", value.Sickness);
+            cmd.Parameters.AddWithValue("allergy", value.Allergy);
+            cmd.Parameters.AddWithValue("healthproblems", value.HealthProblems);
+            cmd.Parameters.AddWithValue("medicationintake", value.MedicationIntake);
+            cmd.Parameters.AddWithValue("foodavoided", value.FoodAvoided);
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+
         }
 
         // PUT api/<ValuesController>/5
